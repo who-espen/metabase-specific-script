e138e2f1b45246da2aa228e9613ab311aff90161
Dyesse YUMBA <yumba.nduba@gmail.com>
2023-07-20 13:22:52 +0100

    add senegal dashboard

---
+++ senegal-metabase/tas/juin 2023/sn-tas1/dashboard/7_innacurrate_gps.sql
@@ -0,0 +1,26 @@
+/*
+ * File: 7_innacurrate_gps.sql
+ * File Created: Tuesday, 5th May 2020 6:36:35 pm
+ * Author: Dyesse YUMBA
+ * Last Modified: Tuesday, 5th May 2020 6:37:59 pm
+ * Modified By: Dyesse YUMBA
+ * -----
+ * (c) 2020, WHO/AFRO/UCN/ESPEN
+ */
+
+/*
+ * This card will list all records with innacurate GPS
+ * Variable to rename <%ab_cde_fgh_1_cluster%>
+ */
+
+SELECT
+    c_recorder "Opérateur",
+    c_eu_name "District",
+    c_cluster_id "Code Site",
+    c_gps_lat "Latitude",
+    c_gps_lng "Longitude",
+    c_gps_acc "Précision",
+    c_start "Date"
+FROM
+    v_espen_sn_lf_tas2_20306_1_site
+WHERE c_gps_acc > 20
